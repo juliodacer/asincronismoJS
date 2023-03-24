ad57f651f587d2e4c2d9f4814cb3fbeda0334d39
Julio Davila <juliodacer@gmail.com>
2023-03-24 07:38:40 -0500

    promise

--- src/Promise/index.js
+++ src/Promise/index.js
@@ -5,9 +5,25 @@
     3. Rechazado
 */
 
-// const promise = new Promise(function(resolve, reject){
-//     resolve('Hey!')
-// })
+// Nos regresa una funcion con 2 funciones dentro (resolve, reject)
+const promise = new Promise(function(resolve, reject){
+    resolve('Hey!')
+})
 
+const cows = 9
+
+const countCows = new Promise(function(resolve, reject){
+    if(cows > 10){
+        resolve(`We have ${cows} cows on the farm`)
+    }else{
+        reject("There no cows on the farm")
+    }    
+})
+
+countCows.then((result) => { 
+    console.log('result:', result) // Se ejecuta el resolve
+}).catch((err) => { // catch: se ejecuta el reject
+    console.log('error:', err)
+}).finally(() => console.log('finally')) // Cuando la ejecucion termino toda ejecucion que puede estar esperando la pr
 
 
